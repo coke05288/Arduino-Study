11f52388c50025d867da7f77e6d1e6194ac64898
YoungJun <coke05288@gmail.com>
2020-08-18 19:18:37 +0900

    Chapter 02-3 | using floating point number

--- Chapter 02/02-3-using_floating_point_numbers/02-3-using_floating_point_numbers.ino
+++ Chapter 02/02-3-using_floating_point_numbers/02-3-using_floating_point_numbers.ino
@@ -1,11 +1,29 @@
-float 
+float value = 1.1;
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  value = value - 0.1;
+  if(value == 0){
+    Serial.println("The value is exactly zeor");
+  }else if(almostEqual(value,0)){
+    Serial.print("The value : ");
+    Serial.print(value);
+    Serial.println(" is almost equal to zero.");
+  }else{
+    Serial.println(value);
+  }
+}
 
+boolean almostEqual(float a, float b){
+
+  const float DELTA = .00001;
+  if (a == 0) return fabs(b) <= DELTA;
+  if (b == 0) return fabs(a) <= DELTA;
+  
+  return fabs((a - b) / max(fabs(a), fabs(b))) <= DELTA;
 }
